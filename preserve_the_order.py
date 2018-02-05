7872765701230e5de7a508a1c33be31d1023ee90
chetanhonnavile <chetanhonnavile@gmail.com>
2018-02-05 12:54:40 -0800

    Update preserve_the_order.py

--- preserve_the_order.py
+++ preserve_the_order.py
@@ -2,6 +2,18 @@
 
 from collections import OrderedDict
 
-a = ['1','2',3,'4','2','1',5,6,9]
-a = list(OrderedDict.fromkeys(a))
-print a  
+
+
+def using_orderedict(li):
+	a = list(OrderedDict.fromkeys(li))
+	return a
+
+def use_append(li):	
+	b = []
+	b = [ i for i in li if i not in b]
+	return b
+
+li = ['1','2',3,'4','2','1',5,6,9]
+
+print using_orderedict(li)
+print "b:",use_append(li)
