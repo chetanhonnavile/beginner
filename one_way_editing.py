0224a90460632b52e81c7eb8ee0fe251ecd04d3d
chetanhonnavile <chetanhonnavile@gmail.com>
2018-02-18 00:10:56 -0800

    Update one_way_editing.py

--- one_way_editing.py
+++ one_way_editing.py
@@ -1,28 +1,28 @@
 def is_one_away_diff_lengths(s1,s2):
     i = 0
     count_diff = 0
-    print "2nd fun"
-    print "s1",s1
-    print "s2",s2
+    #print "2nd fun"
+    #print "s1",s1
+    #print "s2",s2
     while i < len(s2):
 
         if s1[i+count_diff] == s2[i]:
-            print s1[i+count_diff],":",s2[i]
-            print "s1[",i,"+",count_diff,"] == s2[",i,"]"
+            #print s1[i+count_diff],":",s2[i]
+            #print "s1[",i,"+",count_diff,"] == s2[",i,"]"
 
             i += 1
         else:
             count_diff += 1
-            print "count_diff",count_diff
+            #print "count_diff",count_diff
             if count_diff > 1:
-                print count_diff
+                #print count_diff
                 return False
     return True
 
 def is_one_away_same(s1,s2):
     count_diff = 0
-    print "s1", s1
-    print "s2", s2
+    #print "s1", s1
+    #print "s2", s2
     for i in range(len(s1)):
         if s1[i] != s2[i]:
             count_diff += 1
