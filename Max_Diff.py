c5d27437528b398149f46b50151ff5b4f222d4de
chetanhonnavile <chetanhonnavile@gmail.com>
2018-04-17 00:21:18 -0700

    Update Max_Diff.py

--- Max_Diff.py
+++ Max_Diff.py
@@ -1,4 +1,5 @@
 #Maximum difference in an sub array
+#maximum sum
 
 def maximum_diff(list1):
     max_diff = list1[1] - list1[0]
@@ -9,4 +10,17 @@ def maximum_diff(list1):
                 max_diff = list1[j]-list1[i]
     return max_diff
 
+
+def maximum_sum(list1):
+    max_sum = list1[1] + list1[0]
+
+    for i in range(0,len(list1)):
+        for j in range(i+1,len(list1)):
+            if (list1[j]+list1[i]) > max_sum:
+                max_sum = list1[j]+list1[i]
+    return max_sum
+
+
 print maximum_diff([11,2,90,10,110])
+
+print maximum_sum([11,2,90,121,110])
