5a98e88b6f178030aeee2e0fae433c00a4b33310
chetanhonnavile <chetanhonnavile@gmail.com>
2020-03-04 07:27:55 -0800

    Update rooks_are_safe.py

--- rooks_are_safe.py
+++ rooks_are_safe.py
@@ -16,6 +16,34 @@ def rooks_are_safe(chessboard):
             return False
     return True
 
-chessboard = [[1,0,0,0],[0,1,0,0],[0,0,0,1],[0,0,0,1]]
-
+chessboard = [[1,0,0,0],[0,1,0,0],[0,0,0,1],[0,0,0,0]]
+print("=======")
 print(rooks_are_safe(chessboard))
+
+
+def rooks_safe(chessboard):
+    n = len(chessboard)
+    for row in range(n):
+        total_row_count = 0
+        for item in range(n):
+            print(chessboard[row][item]),
+            total_row_count += chessboard[row][item]
+        if total_row_count > 1:
+            return "Rooks are not safe"
+        
+        print("\n")
+    print "======="
+    for col in range(n):
+        total_col_count = 0
+        for item in range(n):
+            print(chessboard[item][col]),
+            total_col_count += chessboard[item][col]
+        if total_col_count > 1:
+            return "Rooks are not safe"
+        
+        print("\n")
+    return "rooks are safe"
+        
+
+print("------------")
+print(rooks_safe(chessboard))
