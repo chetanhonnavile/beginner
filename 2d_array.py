61f4c01fc88376349515f28467eab86d86fa4b42
chetanhonnavile <chetanhonnavile@gmail.com>
2020-03-04 06:38:00 -0800

    Create 2d_array.py

---
+++ 2d_array.py
@@ -0,0 +1,15 @@
+2 dimensional array
+
+a = [[1,3,6,7],
+    [5,4,7,8,]]
+
+for i in a:
+  for j in i:
+    print(j)
+
+
+print("******************")
+
+for row in range(len(a)):
+    for item in range(len(a[row])):
+        print(a
